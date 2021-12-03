2367c763453ea1c18df4ce904665207a1de1ccd8
Conrad Rushing <conrad@costarastrology.com>
2021-12-03 15:05:39 -0500

    Removed old snippet

--- _file_read.py
+++
@@ -1,4 +0,0 @@
-with open('puzzle_input.txt') as input_file:
-    instruction_list = [input_line.strip() for input_line in input_file]
-    for problem_input in instruction_list:
-        problem_output += get_primary_result(int(problem_input))
